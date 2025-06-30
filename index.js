bd8cdb75fad6d84ebbffaf0db51b080ca105e663
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-06-30 14:24:38 +0000

    feat: Add Groq SDK integration and update dependencies in package.json

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import Groq from "groq-sdk";
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function callAIagent() {
+    const messageArray = [
+        {
+  "role": "system",
+  "content": "You are BlogDrop, an expert AI assistant specialized in discovering and analyzing high-quality software engineering blogs. Your task is to help the user find insightful engineering content—such as deep system breakdowns, behind-the-scenes architecture, problem-solving case studies, and real-world engineering challenges—from platforms like Medium, Dev.to, Hacker News, engineering team blogs, and more. Your goal is to boost the user’s technical knowledge and skill set as an aspiring software engineer."
+}
+, {
+            role: "user",
+            content: "who are you?",
+        }
+    ];
+    const completion = await groq.chat.completions.create({
+        messages: messageArray,
+        model: "llama-3.3-70b-versatile",
+    })
+    console.log(JSON.stringify(completion.choices[0], null, 2));
+}
+
+callAIagent();
