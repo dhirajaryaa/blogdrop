0eda1fa5852e9ecfe1dfc57093deb8430d6e1dc0
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-07-02 15:38:56 +0000

    feat: add web scraping functionality with Postlight parser and SerpApi integration
    - Updated package.json to include @postlight/parser and serpapi dependencies.
    - Modified webSearch.js to implement web search using SerpApi and parse the content of the retrieved links using Postlight parser.
    - Adjusted the number of blog links retrieved to 3 and changed the return format to include content and source URL.

--- src/helper/webSearch.js
+++ src/helper/webSearch.js
@@ -1,17 +1,26 @@
+import Parser from '@postlight/parser';
 import { getJson } from "serpapi";
 
 const apiKey = process.env.SERP_API_KEY
 
 export async function searchOnWeb(query) {
+    console.log("Search on web...");
+    
     const response = await getJson({
         engine: "google",
         api_key: apiKey,
         q: query,
     });
-    const blogLinks = response?.organic_results?.slice(0, 5)?.map(data => data.link) || "['No Link found']"
+    const blogLinks = response?.organic_results?.slice(0, 3)?.map(data => data.link) || ["No Link found"];
     // console.log(blogLinks);
-
-    return blogLinks
+    let blogContents = [];
+    console.log("Read web page...");
+    for (const link of blogLinks) {
+        const result = await Parser.parse(link, { contentType: 'markdown' });
+        blogContents.push({ content: result.content, source: result.url });
+    }
+    return blogContents
 
 }
+
 
